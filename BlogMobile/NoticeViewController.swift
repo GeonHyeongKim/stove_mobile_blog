1cec61716dca3e447877394a4f363f6eee4b35cb
김건형 <show11837@gmail.com>
2021-11-03 00:21:40 +0900

    feat : 제목을 변경 할 수 있는 기능
    insertTitleInNavigation 		: navigationItem.titleView에 custom한 button을 넣음
    clickOnTitleButton			: 제목 버튼을 클릭했을때 alert이 발생
    alertTextFieldDidChange	: alert내 textFiled의 내용이 변경할때 발생
    *참고 : textFiled에 "shouldChangeCharactersIn"가 존재함

--- BlogMobile/NoticeViewController.swift
+++ BlogMobile/NoticeViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 class NoticeViewController: UIViewController {
 
     @IBOutlet weak var tvNotice: UITextView!
+    var noticeTitle: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        insertTitleInNavigation("제목")
+    }
+    
+    func insertTitleInNavigation(_ title: String?) {
+        noticeTitle = title
+        let button =  UIButton(type: .custom)
+        button.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(clickOnTitleButton), for: .touchUpInside)
+        navigationItem.titleView = button
+    }
+    
+    @objc func clickOnTitleButton() {
+        let alert = UIAlertController(title: "제목을 설정해주세요", message: "10자 이내", preferredStyle: .alert)
         
+        let okAction = UIAlertAction(title: "저장", style: .destructive) { [weak self] (action) in
+            self?.insertTitleInNavigation(alert.textFields?[0].text)
+        }
+        okAction.isEnabled = false
+        
+        alert.addAction(okAction)
+        alert.addTextField{ [self](textField) in
+            textField.placeholder = noticeTitle
+            textField.addTarget(self, action: #selector(alertTextFieldDidChange), for: .editingChanged)
+        }
+        let cancleAction = UIAlertAction(title: "취소", style: .cancel) { [weak self] (action) in
+            self?.insertTitleInNavigation(self?.noticeTitle)
+        }
+        alert.addAction(cancleAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func alertTextFieldDidChange(){
+        let alertController:UIAlertController = self.presentedViewController as! UIAlertController;
+        let textField :UITextField  = alertController.textFields![0];
+        let addAction: UIAlertAction = alertController.actions[0];
+        addAction.isEnabled = (textField.text?.count)! > 0 && (textField.text?.count)! < 10;
     }
     
     @IBAction func close(_ sender: Any) {
@@ -27,7 +65,7 @@ class NoticeViewController: UIViewController {
             return
         }
         
-        guard let title = navigationItem.title, title.count > 0 else {  // 제목이 입력되지 않았을 경우
+        guard let title = noticeTitle, title.count > 0 else {  // 제목이 입력되지 않았을 경우
             alert(message: "제목을 입력해주세요")
             return
         }
