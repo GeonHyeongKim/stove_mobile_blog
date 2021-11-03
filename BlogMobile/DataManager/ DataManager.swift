b808cd242f73d0bf251a9afa1a64c06eab6261a6
김건형 <show11837@gmail.com>
2021-11-03 16:25:19 +0900

    feat : 싱글톤 패턴으로 객체의 instance 1개만 생성해서 데이터 다루기
    싱글톤(Singleton) 패턴의 정의는 단순하다. 객체의 인스턴스가 오직 1개만 생성되는 패턴을 의미한다.
    사용한 이유 :
    1) 메모리측면
    2) 데이터 공유가 쉽다는점

---
+++ BlogMobile/DataManager/ DataManager.swift
@@ -0,0 +1,45 @@
+//
+//   DataManager.swift
+//  BlogMobile
+//
+//  Created by gunhyeong on 2021/11/03.
+//
+
+import Foundation
+
+import CoreData
+
+class DataManager {
+    static let shared = DataManager()
+    private init() {
+        
+    }
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "BlogMobile")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
+
